e17979cc65eb2b5d5cd2507e4c8e1a9e2a3f4588
Yan Alejandro <ysilva@idealit.com.br>
2021-11-23 16:05:01 -0300

    adiciona projeto no repositorio

---
+++ AlphaTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlphaTech
+//
+//  Created by Yan Alejandro on 23/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
